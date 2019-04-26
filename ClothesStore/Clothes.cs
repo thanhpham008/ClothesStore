282f4600192719555968745fa2bc83e372d36fdb
Premier <Premier@WIN10>
2019-04-26 22:34:50 +0700

    Add project files.

---
+++ ClothesStore/Clothes.cs
@@ -0,0 +1,124 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothesStore
+{
+	public class Store
+	{
+		public List<Clothes> Clothes { get; set; }
+
+		public int Money { get; set; }
+
+		public Store()
+		{
+			Clothes = new List<Clothes>();
+			Money = 1000;
+		}
+
+		public void Buy(Clothes clothes, int quantity)
+		{
+			var clothe = Clothes.FirstOrDefault(x => x.Type == clothes.Type && x.Size == clothes.Size && x.Color == clothes.Color);
+			if (clothe != null)
+			{
+				clothe.Quantity += quantity;
+				Money -= clothes.BuyPrice * quantity;
+			}
+			else
+			{
+				clothes.Quantity = quantity;
+				Clothes.Add(clothes);
+				Money -= clothes.BuyPrice * quantity;
+			}
+		}
+
+		public int Sell(Clothes clothes, int quantity)
+		{
+			var clothe = Clothes.FirstOrDefault(x => x.Type == clothes.Type && x.Size == clothes.Size && x.Color == clothes.Color);
+			if (clothe != null)
+			{
+				if (clothe.Quantity >= quantity)
+				{
+					clothe.Quantity -= quantity;
+					Money += clothes.SellPrice * quantity;
+					return 0;
+				}
+				else
+				{
+					return 1;
+				}
+			}
+			return 2;
+		}
+	}
+
+	public class Clothes
+	{
+		public int BuyPrice { get; set; }
+		public int SellPrice { get; set; }
+		public Type Type { get; set; }
+		public Color Color { get; set; }
+		public Size Size { get; set; }
+		public int Quantity { get; set; }
+
+		public virtual void Init(Type type, Color color, Size size)
+		{
+			Type = type;
+			Color = color;
+			Size = size;
+		}
+	}
+
+	public class TShirt : Clothes
+	{
+		public override void Init(Type type, Color color, Size size)
+		{
+			BuyPrice = 6;
+			SellPrice = 12;
+			base.Init(type, color, size);
+		}
+
+	}
+
+	public class DShirt : Clothes
+	{
+		public override void Init(Type type, Color color, Size size)
+		{
+			BuyPrice = 8;
+			SellPrice = 20;
+			base.Init(type, color, size);
+		}
+	}
+
+	public class Jean : Clothes
+	{
+		public override void Init(Type type, Color color, Size size)
+		{
+			BuyPrice = 10;
+			SellPrice = 30;
+			base.Init(type, color, size);
+		}
+	}
+
+	public enum Type
+	{
+		TShirt = 1,
+		DressShirt = 2,
+		Jean = 3
+	}
+
+	public enum Size
+	{
+		Small = 1,
+		Medium = 2,
+		Large = 3
+	}
+
+	public enum Color
+	{
+		Red = 1,
+		Green = 2,
+		Yellow = 3,
+		Blue = 4
+	}
+}
---
+++ ClothesStore/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace ClothesStore
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+			var store = new Store();
+
+			var tShirt = new TShirt();
+			tShirt.Init(Type.TShirt, Color.Red, Size.Small);
+			store.Buy(tShirt, 1);
+
+			var dressShirt = new DShirt();
+			dressShirt.Init(Type.DressShirt, Color.Green, Size.Medium);
+			store.Buy(dressShirt, 1);
+
+			var jean = new Jean();
+			jean.Init(Type.Jean, Color.Blue, Size.Large);
+			store.Buy(jean, 1);
+
+			tShirt = new TShirt();
+			tShirt.Init(Type.TShirt, Color.Yellow, Size.Small);
+			store.Buy(tShirt, 1);
+
+			tShirt = new TShirt();
+			tShirt.Init(Type.TShirt, Color.Yellow, Size.Small);
+			store.Buy(tShirt, 1);
+
+			//store.Sell(tShirt);
+
+
+			//tShirt = new TShirt();
+			//tShirt.Init(6, 12, Type.TShirt, Color.Red);
+
+			//store.Clothes = store.Clothes.OrderBy(x => x.Type).ToList();
+			Console.WriteLine("Welcome to ClothesStore.");
+			PrintStoreStatus(store);
+
+			while (true)
+			{
+				Console.Write("Do you want to buy or sell (1/2)?: ");
+				var action = Console.ReadLine();
+				if (action == "1")
+				{
+					Console.Write("Which type of clothes do you want to buy? (TShirt: 1, DressShirt: 2, Jean: 3): ");
+					var type = Console.ReadLine();
+					Console.Write("Please choose color (Red: 1, Green: 2, Yellow: 3, Blue: 4): ");
+					var color = Console.ReadLine();
+					Console.Write("Please choose size (Small: 1, Medium: 2, Large: 3): ");
+					var size = Console.ReadLine();
+					Console.Write("How many items do you want to buy?: ");
+					var quantity = Console.ReadLine();
+
+					var item = new TShirt();
+					item.Init((Type)int.Parse(type), (Color)int.Parse(color), (Size)int.Parse(size));
+					store.Buy(item, int.Parse(quantity));
+				}
+				else if (action == "2")
+				{
+					Console.Write("Choose the type of clothes you want to sell (TShirt: 1, DressShirt: 2, Jean: 3): ");
+					var type = Console.ReadLine();
+					Console.Write("Please choose color (Red: 1, Green: 2, Yellow: 3, Blue: 4): ");
+					var color = Console.ReadLine();
+					Console.Write("Please choose size (Small: 1, Medium: 2, Large: 3): ");
+					var size = Console.ReadLine();
+					Console.Write("How many items do you want to sell?: ");
+					var quantity = Console.ReadLine();
+
+					var item = new TShirt();
+					item.Init((Type)int.Parse(type), (Color)int.Parse(color), (Size)int.Parse(size));
+					var sell = store.Sell(item, int.Parse(quantity));
+					if (sell == 0)
+						Console.WriteLine("Sell succesfully!");
+					else if (sell ==1)
+						Console.WriteLine("Sorry, this item is out of stock!");
+					else if (sell == 2)
+						Console.WriteLine("Sorry, cannot find this item in stock!");
+				}
+
+				PrintStoreStatus(store);
+			}
+
+		}
+
+		private static void PrintStoreStatus(Store store)
+		{
+			Console.WriteLine("Store status is:");
+			Console.WriteLine("======================");
+
+			foreach (var clothe in store.Clothes)
+			{
+				Console.WriteLine("Type: " + clothe.Type.ToString());
+				Console.WriteLine("Color: " + clothe.Color.ToString());
+				Console.WriteLine("Size: " + clothe.Size.ToString());
+				Console.WriteLine("Buying price: " + clothe.BuyPrice.ToString());
+				Console.WriteLine("Selling price: " + clothe.SellPrice.ToString());
+				Console.WriteLine("Quantity: " + clothe.Quantity);
+				Console.WriteLine("Total money left in store: " + store.Money);
+
+				Console.WriteLine();
+			}
+			Console.WriteLine("======================");
+		}
+	}
+}
